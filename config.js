aa8c390ac8c815fb2f6b73412fd841a151207431
gregoryisaacs803-alt <gregoryisaacs803@gmail.com>
2026-09-16 13:50:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ Site: https://www.movanest.xyz
 
 global.session_id = "" //Input your session id like deploy with railway or heroku. | If you deploy with pterodactyl panel don't add session id.
 global.prefix = '.' // bot prefix
-global.owner = ["9476XXXXX"] // Add Your Owner number
-global.bot = "9476XXXXX" // Add Your Bot number
+global.owner = ["254757436134"] // Add Your Owner number
+global.bot = "254757436134" // Add Your Bot number
 global.namabot = "X-Crasher" // Add Your Bot name
 global.namaown = "DanuZz" // Add Your Bot owner name
 global.workType = "public" // public, private, groups, inbox
